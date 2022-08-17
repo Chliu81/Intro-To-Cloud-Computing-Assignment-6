45c952fc266fb8a808e26b816de570ccbb215957
Chliu88 <71086498+Chliu88@users.noreply.github.com>
2022-08-17 16:10:03 -0400

    Update cc-index-photos.py

--- lambdas/cc-index-photos.py
+++ lambdas/cc-index-photos.py
@@ -88,5 +88,5 @@ def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Hi from Lambda!')
     }
